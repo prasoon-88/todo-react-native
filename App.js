353b4ea9701a29603a5fd563953c82741ba43ca8
prasoonasati-sigfyn <165634066+prasoonasati-sigfyn@users.noreply.github.com>
2024-05-05 11:22:28 +0530

    init commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import Color from './App/Utils/Color';
+import { useState } from 'react';
 
 export default function App() {
+  const [todo,setTodo] = useState([])
+  const [input,setInput]  = useState('')
+
+  const addTodo = ()=>{
+    if(input == ''){
+      return 
+    }
+    setTodo([input,...todo])
+    setInput('')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Todo List</Text>
+      <TextInput style={styles.input} value={input} onChangeText={(text)=>setInput(text)}/>
+      <Button title='Add Todo' onPress={addTodo}/>
+      <View style={styles.list}>
+        {
+          todo?.map((todo,index)=>
+          <View>
+              <Text style={styles.todo} key={index}>{`${index+1}  ` + todo} <Button color="#841584" title="Delete"/></Text>
+          </View>
+        )
+        }
+      </View>
+      <StatusBar/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    display:'flex',
+    padding:24,
+    marginTop:10
   },
-});
+  heading:{
+    fontSize:20,
+    marginBottom:6,
+    fontWeight:'bold'
+  },
+  input:{
+    fontSize:20,
+    borderWidth:1,
+    borderColor:Color.BLACK,
+    height:50,
+    width:'100%',
+    borderRadius:6,
+    padding:12,
+    marginBottom:10
+  },
+  list:{
+    marginTop:10,
+  }
+  ,
+  todo:{
+    fontSize:20,
+    display:'flex',
+    alignContent:'center',
+    justifyContent:'center'
+  },
+  
+})
+
---
+++ App/Screens/LoginScreen/Login.jsx
@@ -0,0 +1,13 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+
+export default function Login() {
+  return (
+    <View >
+      <Text>Login</Text>
+      <Image source={require('../../../assets/icon.png')}/>
+    </View>
+  )
+}
+
+  
